7b06e6c02f394519edd01f3106a4db5ad23cff1b
shreyashm18 <musaleshreyash@gmail.com>
2019-10-09 23:45:48 +0530

    Dynamic content able to show, basic class and just creating object in views.py file

--- trip/models.py
+++ trip/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class destination:
+    
+    name=str
+    img=str
+    desc=str
+    price=int
--- trip/views.py
+++ trip/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-
+from .models import destination
 # Create your views here.
 def triphome(request):
-    return render(request,'index.html')
+
+    dest1=destination()
+    dest1.price=800
+    dest1.name='Pune'
+    dest1.desc='peshve city never lose'
+    dest1.img='destination_4.jpg'
+
+    dest2=destination()
+    dest2.price=400
+    dest2.name='Mumbai'
+    dest2.desc='capital'
+    dest2.img='destination_6.jpg'
+
+    dest3=destination()
+    dest3.price=700
+    dest3.name='delhi'
+    dest3.desc='polluted'
+    dest3.img='destination_3.jpg'
+    dest=[dest1,dest2,dest3]
+    return render(request,'index.html',{'Dest':dest})
